7a382c56084d8426d293505a828914fad1ed3d9c
Kepik <officialkepik@gmail.com>
2026-04-21 21:00:40 +0500

    init

---
+++ src/main/java/uladzislau/kapusto/leetcode/Challenge365DaysLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package uladzislau.kapusto.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge365DaysLeetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Challenge365DaysLeetcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uladzislau/kapusto/leetcode/Challenge365DaysLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package uladzislau.kapusto.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge365DaysLeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
